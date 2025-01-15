d74d96d1a697f27be6486111f2d7c1d2b99ff145
Joao <junioralmeida_15@hotmail.com>
2025-01-14 21:18:56 -0300

    criação da fila de pedidos e teste de consumo com logging

--- src/main/java/spring/api/orderms/listener/OrderCreatedListener.java
+++ src/main/java/spring/api/orderms/listener/OrderCreatedListener.java
@@ -1,5 +1,7 @@
 package spring.api.orderms.listener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
@@ -10,8 +12,10 @@ import spring.api.orderms.listener.dto.OrderCreatedEvent;
 @Component
 public class OrderCreatedListener {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     @RabbitListener(queues = RabbitMqConfig.ORDER_CREATED_QUEUE)
     public void listen(Message<OrderCreatedEvent> message) {
-
+        logger.info("Message consumed: {}", message);
     }
 }
